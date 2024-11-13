0ad7505a62882310e56b9c87da43580228018f16
daffaalex22 <daffaalex22@gmail.com>
2024-11-14 00:41:35 +0700

    Get each product's title

--- index.js
+++ index.js
@@ -5,30 +5,36 @@ const puppeteer = require('puppeteer-core');
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe",
-    headless: false
+    headless: false,
+    defaultViewport: false
   });
   const page = await browser.newPage();
 
   // Navigate the page to a URL.
-  await page.goto('https://developer.chrome.com/');
+  await page.goto('https://www.tokopedia.com/find/pc-rakitan?source=homepage.popular_keywords.0.0&srp_component_id=06.01.01.04');
 
-  // Set screen size.
-  await page.setViewport({width: 1080, height: 1024});
+  await page.waitForSelector(".css-1vknpta")
+  await page.waitForSelector(".css-15vayma")
 
-  // Type into search box.
-  await page.locator('.devsite-search-field').fill('automate beyond recorder');
+  const titles = await page.$$('.css-1vknpta > .css-15vayma');
 
-  // Wait and click on first result.
-  await page.locator('.devsite-result-item-link').click();
+  let productCount = 0;
+  for (const title of titles) {
+    let text = "NULL"
+    
+    try {
+      text = await page.evaluate(
+        (el) => el.textContent,
+        title
+      );
+    } catch (error) {}
 
-  // Locate the full title with a unique string.
-  const textSelector = await page
-    .locator('text/Customize and automate')
-    .waitHandle();
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
+    if (text !== "NULL" && text.length > 0) {
+      productCount++
+    }
+  }
 
-  // Print the full title.
-  console.log('The title of this blog post is "%s".', fullTitle);
+  console.log("Number of Products:", productCount)
 
   // await browser.close();
 })()
